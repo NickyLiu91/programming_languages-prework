8fbddfbfd8467493d656a3a1acd531b1011d4f2f
Nicky Liu <nickyliu91@gmail.com>
2018-08-22 12:53:52 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,7 +2,7 @@ def reformat_languages(languages)
   # your code here
   hash = {}
   languages.each do |key, value|
-    key.each do |language, type|
+    value.each do |language, type|
       hash[language] = type
       hash[language] = {:style => key}
     end
